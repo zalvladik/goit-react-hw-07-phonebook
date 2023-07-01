6edfaeb63852bea5395b3ffcc86dcbcffc886d8c
zalvladik <zalipkav@ukr.net>
2023-07-02 01:12:41 +0300

    kk

--- src/components/redux/selectors.js
+++ src/components/redux/selectors.js
@@ -1,5 +1,3 @@
-import { createSelector } from "@reduxjs/toolkit";
-
 export const reduxContacts = state => state.contacts.items; 
 export const reduxIsLoading = state => state.contacts.isLoading;
 export const reduxError = state => state.contacts.error;
